8a6bdebdc18fb8d57ed3e408388d8be71fac10ed
budancamanak <budancamanak@gmail.com>
2025-03-02 00:41:12 +0300

    updated Market.API startup & Dockerfile

--- src/Market/Market.API/Program.cs
+++ src/Market/Market.API/Program.cs
@@ -64,11 +64,11 @@ builder.Services.AddGrpc(options =>
 });
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenLocalhost(5238,
+    options.ListenAnyIP(5238,
         listenOptions => { listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2; });
 
     // HTTP/1.1 for other endpoints (e.g., MVC or REST)
-    options.ListenLocalhost(5237, listenOptions =>
+    options.ListenAnyIP(5237, listenOptions =>
     {
         listenOptions.Protocols =
             Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols
