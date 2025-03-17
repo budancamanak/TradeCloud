de0346a7b3440b10040a1dd57186062beee9b272
budancamanak <budancamanak@gmail.com>
2025-03-17 22:31:43 +0300

    run analysis execution feature started

---
+++ src/Backend/Backend.Application/Features/Execution/RunAnalysisExecution/RunAnalysisExecutionRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using Common.Core.Models;
+using MediatR;
+
+namespace Backend.Application.Features.Execution.RunAnalysisExecution;
+
+public class RunAnalysisExecutionRequestHandler : IRequestHandler<RunAnalysisExecutionRequest, MethodResponse>
+{
+    public Task<MethodResponse> Handle(RunAnalysisExecutionRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
