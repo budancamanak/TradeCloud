ff4fe3ab3e6c7a1015924058d5361ac569538310
budancamanak <budancamanak@gmail.com>
2025-04-12 20:59:25 +0300

    initial cached math operation. added GetSma and GetAdl
    Signed-off-by: budancamanak <budancamanak@gmail.com>

---
+++ src/Common/Common.Plugin/Math/TradeMath.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using Common.Application.Repositories;
+using Common.Core.DTOs;
+using Skender.Stock.Indicators;
+
+namespace Common.Plugin.Math;
+
+public class TradeMath
+{
+    private readonly ICacheService cache;
+    private readonly List<Quote> prices;
+    private readonly int analysisExecutionId;
+
+    public TradeMath(ICacheService cache, int analysisExecutionId, List<PriceDto> priceDtos)
+    {
+        this.analysisExecutionId = analysisExecutionId;
+        this.cache = cache;
+        this.prices = priceDtos.ToQuotes();
+    }
+
+    private TOut CachedFunc<TOut>(string cacheKey, Func<TOut> func)
+    {
+        var cached = cache.GetAsync<TOut>(cacheKey).GetAwaiter().GetResult();
+        if (cached != null) return cached;
+        cached = func();
+        cache.SetAsync(cacheKey, cached, TimeSpan.FromMinutes(30)).GetAwaiter().GetResult();
+        return cached;
+    }
+
+    private string GenerateCacheKey(string prefix, params object[]? args)
+    {
+        StringBuilder sb = new StringBuilder(prefix);
+        if (args != null)
+        {
+            sb.Append("_");
+            foreach (var value in args)
+            {
+                sb.Append(value).Append("_");
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public IEnumerable<SmaResult> GetSma(int lookbackPeriods)
+    {
+        var key = GenerateCacheKey("GET_SMA", analysisExecutionId, lookbackPeriods);
+        return CachedFunc(key, () => prices.GetSma(lookbackPeriods));
+    }
+    public IEnumerable<AdlResult> GetAdl(int smaPeriods)
+    {
+        var key = GenerateCacheKey("GET_SMA", analysisExecutionId, smaPeriods);
+        return CachedFunc(key, () => prices.GetAdl(smaPeriods));
+    }
+}
--- tests/Worker/Infrastructure.Tests/UnitTest1.cs
+++ tests/Worker/Infrastructure.Tests/UnitTest1.cs
@@ -1,6 +1,9 @@
+using System.Linq.Expressions;
+using System.Text;
 using Common.Plugin.Models;
 using FluentAssertions;
 using Newtonsoft.Json;
+using StackExchange.Redis;
 using Worker.Plugins.MovingAverage;
 
 namespace Infrastructure.Tests;
@@ -12,7 +15,57 @@ public class Tests
     {
     }
 
+    protected TOut CachedFunc<TOut>(Func<TOut> func)
+    {
+        Console.WriteLine("Called CachedFunc on . {}", func.ToString());
+        Console.WriteLine(">> func.Target : {}", func.Target?.ToString());
+        Console.WriteLine(">> func.Method : {}", func.Method.ToString());
+        return func();
+    }
+
+    protected TOut CachedFunc2<TOut>(Expression<TOut> func)
+    {
+        var x = func.Compile();
+        return x;
+    }
+
+
+    private int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+    private string GenerateCacheKey(string prefix, params object[]? args)
+    {
+        StringBuilder sb = new StringBuilder(prefix);
+        if (args != null)
+        {
+            sb.Append("_");
+            foreach (var value in args)
+            {
+                sb.Append(value).Append("_");
+            }
+        }
+
+        return sb.ToString();
+    }
+
     [Test]
+    public void TestFunc()
+    {
+        // var x = CachedFunc2(() => Multiply(3, 5));
+        // var f = () => Multiply(3, 5);
+        // Expression<Func<int>> bind = () => f();
+        // var a = bind.Compile().Invoke();
+        // var x = CachedFunc2(bind).Invoke();
+        string key = GenerateCacheKey("PREF", 1, 3, 4, null, 6.0);
+        Console.WriteLine(key);
+        var x = 3;
+        Assert.IsTrue(x == 3);
+    }
+
+    [Test]
+    [Ignore("skip for fast")]
     public void Test1()
     {
         var paramSet = new GoldenDeathCrossPluginParams(50, 200);
