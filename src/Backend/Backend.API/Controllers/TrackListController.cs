697cfb781af16040518f7d117fda6ffa35edd5f9
budancamanak <budancamanak@gmail.com>
2025-04-16 01:32:09 +0300

    Modified Rest api paths
    Signed-off-by: budancamanak <budancamanak@gmail.com>

--- src/Backend/Backend.API/Controllers/TrackListController.cs
+++ src/Backend/Backend.API/Controllers/TrackListController.cs
@@ -16,7 +16,7 @@ public class TrackListController(
     ILogger<TrackListController> logger,
     IMediator mediator)
 {
-    [HttpGet("GetAvailableTickers")]
+    [HttpGet("/AvailableTickers")]
     public async Task<List<TickerDto>> GetAvailableTickers()
     {
         var request = new ListAvailableTickersRequest();
@@ -24,23 +24,24 @@ public class TrackListController(
         return result;
     }
 
-    [HttpGet("GetUserTrackList")]
+    [HttpGet]
     public async Task<List<TrackListDto>> GetUserTrackList([FromQuery] ListUserTrackListRequest request)
     {
         var result = await mediator.Send(request);
         return result;
     }
 
-    [HttpPost("AddTickerToUserTrackList")]
+    [HttpPost]
     public async Task<MethodResponse> AddTickerToUserTrackList([FromBody] AddTickerToTrackListRequest request)
     {
         var result = await mediator.Send(request);
         return result;
     }
 
-    [HttpDelete("RemoveTickerFromUserTrackList")]
-    public async Task<MethodResponse> RemoveTickerFromUserTrackList([FromBody] RemoveUserTrackListRequest request)
+    [HttpDelete("User/{userId:int}/Ticker/{tickerId:int}")]
+    public async Task<MethodResponse> RemoveTickerFromUserTrackList(int userId, int tickerId)
     {
+        var request = new RemoveUserTrackListRequest { UserId = userId, TickerId = tickerId };
         var mr = await mediator.Send(request);
         return mr;
     }
