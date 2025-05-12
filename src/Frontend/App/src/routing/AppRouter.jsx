f632e0877a74c1fe27bc9c5234fa89442b299cc0
budancamanak <budancamanak@gmail.com>
2025-05-12 21:46:35 +0300

    removed comments

--- src/Frontend/App/src/routing/AppRouter.jsx
+++ src/Frontend/App/src/routing/AppRouter.jsx
@@ -1,21 +1,9 @@
 import { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Layout from "../features/layout/Layout"
-// import Login from "../features/user/Login"
-// import Statistics from "../features/statistics/Statistics"
-// import Consultation from "../features/consultation/Consultation"
-// import ExternalConsultation from "../features/consultation/ExternalConsultation"
-// import { Cookies } from "react-cookie";
 import BaseLayout from "../layouts/BaseLayout";
-import Login from "../features/auth/login/Login"
-// import ForgotPassword from "../features/user/ForgotPassword"
-// import ResetPassword from "../features/user/ResetPassword"
-// import Deidentifies from "../features/deidentify/Deidentifies"
-// import SlideViewer from "../features/slideviewer/SlideViewer"
-// import ExternalSlidesView from "../features/consultation/ExternalSlides.view"
+import Login from "../features/auth/login/Login";
 
 function AppRouter() {
-  // const cookies = new Cookies();
   return (
     <>
       <Router>
@@ -23,12 +11,7 @@ function AppRouter() {
           <Route
             path="/"
             element={
-              localStorage.getItem("access_token") ? (
-                <BaseLayout                  
-                />
-              ) : (
-                <Login />
-              )
+              localStorage.getItem("access_token") ? <BaseLayout /> : <Login />
             }
           />
 
