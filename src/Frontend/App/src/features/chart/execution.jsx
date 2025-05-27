d17c938f87fe382e475b375bae44c12f5c38ba42
budancamanak <budancamanak@gmail.com>
2025-05-27 03:16:09 +0300

    Added annotations. need more work
    Signed-off-by: budancamanak <budancamanak@gmail.com>

--- src/Frontend/App/src/features/chart/execution.jsx
+++ src/Frontend/App/src/features/chart/execution.jsx
@@ -3,11 +3,91 @@ import { Route, useParams } from "react-router-dom";
 import ReactECharts from "echarts-for-react";
 import Fetcher from "../../utils/network";
 import dayjs from "dayjs";
+import { cloneDeep } from "lodash";
+
 const upColor = "#ec0000";
 const upBorderColor = "#8A0000";
 const downColor = "#00da3c";
 const downBorderColor = "#008F28";
 function ExecutionChart() {
+  const default_option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "cross",
+      },
+    },
+    grid: {
+      left: "0%",
+      right: "5%",
+      bottom: "5%",
+    },
+    xAxis: {
+      type: "category",
+      data: [],
+      boundaryGap: false,
+      axisLine: { onZero: false },
+      splitLine: { show: false },
+      min: "dataMin",
+      max: "dataMax",
+    },
+    yAxis: {
+      scale: true,
+      position: "right",
+      splitArea: {
+        show: true,
+      },
+    },
+  dataZoom: [
+    {
+      textStyle: {
+        color: '#8392A5'
+      },
+      handleIcon:
+        'path://M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      dataBackground: {
+        areaStyle: {
+          color: '#8392A5'
+        },
+        lineStyle: {
+          opacity: 0.8,
+          color: '#8392A5'
+        }
+      },
+      brushSelect: true
+    },
+    {
+      type: 'inside'
+    }
+  ],
+    series: [
+      {
+        name: "ExecutionChart",
+        type: "candlestick",
+        data: [],
+        itemStyle: {
+          color: upColor,
+          color0: downColor,
+          borderColor: upBorderColor,
+          borderColor0: downBorderColor,
+        },
+        markPoint: {
+          label: {
+            formatter: function (param) {
+              return param != null ? Math.round(param.value) + "" : "";
+            },
+          },
+          data: [],
+          tooltip: {
+            formatter: function (param) {
+              return param.name + "<br>" + (param.data.coord || "");
+            },
+          },
+        },
+      },
+    ],
+  };
+  const [option, setOption] = useState(default_option);
   const { executionId } = useParams();
   const [prices, setPrices] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -15,12 +95,11 @@ function ExecutionChart() {
   const [longCount, setLongCount] = useState(0);
   const [shortCount, setShortCount] = useState(0);
   const [analysis, setAnalysis] = useState(null);
-  const chartRef = useRef(null);
+
   const fetcher = new Fetcher();
 
   useEffect(() => {
     fetcher.get("Chart/Execution/" + executionId + "/Prices").then((result) => {
-      console.log("got prices", result);
       setPrices(result);
     });
     fetcher.get(`AnalysisExecutions/${executionId}/Details`).then((result) => {
@@ -48,11 +127,41 @@ function ExecutionChart() {
     });
   }, [executionId]);
   useEffect(() => {
+    if (
+      !analysis ||
+      analysis.pluginExecutions == null ||
+      analysis.pluginExecutions.length <= 0
+    )
+      return;
+    const newOption = cloneDeep(option);
+    // analysis.pluginExecutions.forEach((exec) => {
+    analysis.pluginExecutions[0].outputs.forEach((signal) => {
+      newOption.series[0].markPoint.data.push({
+        name: "Mark",
+        coord: [dayjs(signal.signalDate).format("DD/MM/YYYY HH:mm"), 88300],
+        value: 8,
+        // valueDim: "lowest",
+        itemStyle: {
+          color: "rgb(41,60,85)",
+        },
+      });
+    });
+    // });
+    // newOption.series[0].markPoint.data.push({
+    //   name: "lowest value",
+    //   type: "min",
+    //   valueDim: "lowest",
+    // });
+
+    setOption(newOption);
+  }, [analysis]);
+
+  useEffect(() => {
     const categoryData = [];
     const values = [];
     for (var i = 0; i < prices.length; i++) {
       // open，close，lowest，highest
-      categoryData.push(prices[i].timestamp);
+      categoryData.push(dayjs(prices[i].timestamp).format("DD/MM/YYYY HH:mm"));
       values.push([
         prices[i].open,
         prices[i].close,
@@ -62,101 +171,11 @@ function ExecutionChart() {
     }
     setCategories(categoryData);
     setValues(values);
+    const newOption = cloneDeep(option);
+    newOption.series[0].data = values;
+    newOption.xAxis.data = categoryData;
+    setOption(newOption);
   }, [prices]);
-
-  const option = {
-    tooltip: {
-      trigger: "axis",
-      axisPointer: {
-        type: "cross",
-      },
-    },
-    grid: {
-      left: "2%",
-      right: "2%",
-      bottom: "2%",
-    },
-    xAxis: {
-      type: "category",
-      data: categories,
-      boundaryGap: false,
-      axisLine: { onZero: false },
-      splitLine: { show: false },
-      min: "dataMin",
-      max: "dataMax",
-    },
-    yAxis: {
-      scale: true,
-      position: "right",
-      splitArea: {
-        show: true,
-      },
-    },
-    dataZoom: [
-      {
-        type: "inside",
-        start: 50,
-        end: 100,
-      },
-      {
-        show: true,
-        type: "slider",
-        top: "90%",
-        start: 50,
-        end: 100,
-      },
-    ],
-    series: [
-      {
-        name: "SERIESTEXT",
-        type: "candlestick",
-        data: values,
-        itemStyle: {
-          color: upColor,
-          color0: downColor,
-          borderColor: upBorderColor,
-          borderColor0: downBorderColor,
-        },
-        markPoint: {
-          label: {
-            formatter: function (param) {
-              return param != null ? Math.round(param.value) + "" : "";
-            },
-          },
-          data: [
-            {
-              name: "Mark",
-              coord: ["2013/5/31", 12300],
-              value: 2300,
-              itemStyle: {
-                color: "rgb(41,60,85)",
-              },
-            },
-            {
-              name: "highest value",
-              type: "max",
-              valueDim: "highest",
-            },
-            {
-              name: "lowest value",
-              type: "min",
-              valueDim: "lowest",
-            },
-            {
-              name: "average value on close",
-              type: "average",
-              valueDim: "close",
-            },
-          ],
-          tooltip: {
-            formatter: function (param) {
-              return param.name + "<br>" + (param.data.coord || "");
-            },
-          },
-        },
-      },
-    ],
-  };
 
   return (
     <>
@@ -245,7 +264,7 @@ function ExecutionChart() {
             </div>
           </div>
           <div className="card-body">
-            <ReactECharts option={option} ref={chartRef} style={{ height: "600px" }} />
+            <ReactECharts option={option} style={{ height: "600px" }} />
           </div>
         </div>
       </div>
