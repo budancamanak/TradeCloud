fb428a2eb2ec08809647b4bd7db1d7609b00a1c5
budancamanak <budancamanak@gmail.com>
2025-06-12 22:36:25 +0300

    Adding wave trend indicator
    Signed-off-by: budancamanak <budancamanak@gmail.com>

---
+++ src/Worker/Worker.Plugins/WaveTrend/WaveTrendPlugin.cs
@@ -0,0 +1,39 @@
+﻿using Common.Application.Repositories;
+using Common.Core.Models;
+using Common.Plugin.Abstraction;
+using Microsoft.Extensions.Logging;
+
+namespace Worker.Plugins.WaveTrend;
+
+public class WaveTrendPlugin(
+    ILogger<IPlugin> logger,
+    IPluginMessageBroker messageBroker,
+    IPluginStateManager stateManager,
+    ICacheService cache)
+    : PluginBase<WaveTrendPluginParams>(logger, messageBroker, stateManager, cache)
+{
+    protected override WaveTrendPluginParams ParseParams(string? json)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override PluginInfo GetPluginInfo()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IParameters GetDefaultParamSet()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Type GetPluginType()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Worker/Worker.Plugins/WaveTrend/WaveTrendPluginParams.cs
@@ -0,0 +1,34 @@
+﻿using Common.Plugin.Abstraction;
+
+namespace Worker.Plugins.WaveTrend;
+
+public class WaveTrendPluginParams: IParameters
+{
+    public IPluginParamSet GetParamSet()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetStringRepresentation()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ToJson()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class WaveTrendPluginParamSet : IPluginParamSet
+{
+    public string GetStringRepresentation()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ToJson()
+    {
+        throw new NotImplementedException();
+    }
+}
